114693354fedd91a91db45da47089fe998507f39
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-26 22:17:04 +0700

    2 files modified

--- components/Tabbar.vue
+++ components/Tabbar.vue
@@ -1,40 +1,42 @@
 
 <template>
-<view class="flex diygw-col-24 diygw-bottom">
-			<view class="diygw-grid diygw-actions">
-				<button class="diygw-action">
-					<view class="diygw-grid-inner">
-						<view class="diygw-grid-icon diygw-avatar" style="font-size: calc(35px - 12px); width: 35px; height: 35px">
-							<image mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png"></image>
-						</view>
-						<view class="diygw-grid-title"> หน้าหลัก </view>
-					</view>
-				</button>
-				<button class="diygw-action">
-					<view class="diygw-grid-inner">
-						<view class="diygw-grid-icon diygw-avatar" style="font-size: calc(35px - 12px); width: 35px; height: 35px">
-							<image mode="aspectFit" class="diygw-avatar-img" src="/static/grid2.png"></image>
-						</view>
-						<view class="diygw-grid-title"> 菜单二 </view>
-					</view>
-				</button>
-				<button class="diygw-action">
-					<view class="diygw-grid-inner">
-						<view class="diygw-grid-icon diygw-avatar" style="font-size: calc(35px - 12px); width: 35px; height: 35px">
-							<image mode="aspectFit" class="diygw-avatar-img" src="/static/grid3.png"></image>
-						</view>
-						<view class="diygw-grid-title"> 菜单三 </view>
-					</view>
-				</button>
-				<button class="diygw-action">
-					<view class="diygw-grid-inner">
-						<view class="diygw-grid-icon diygw-avatar" style="font-size: calc(35px - 12px); width: 35px; height: 35px">
-							<image mode="aspectFit" class="diygw-avatar-img" src="/static/grid4.png"></image>
-						</view>
-						<view class="diygw-grid-title"> 菜单四 </view>
-					</view>
-				</button>
-			</view>
-		</view>
+
+<div class="flex diygw-col-24 diygw-bottom">
+					<div class="diygw-grid diygw-actions">
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png" />
+								</div>
+								<div class="diygw-grid-title">หน้าหลัก</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid2.png" />
+								</div>
+								<div class="diygw-grid-title">ภารกิจ</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid3.png"/>
+								</div>
+								<div class="diygw-grid-title">แชท</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid4.png"/>
+								</div>
+								<div class="diygw-grid-title">ข้อมูล</div>
+							</div>
+						</button>
+					</div>
+				</div>
+
 
 </template>
